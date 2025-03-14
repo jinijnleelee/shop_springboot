9b9df5deb57f783899ffaa3b5b4a46dba96c4c89
LEE JINYOUNG <jinijnleelee@gmail.com>
2025-03-14 21:29:14 +0900

    List/RequiredArgsConstructor

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -2,17 +2,38 @@ package com.apple.shop;
 //클래스를 다른 파일에서 쓰고싶으면 위에 있는 이 파일 경로를 적어놔야 함
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RequiredArgsConstructor
 @Controller
 public class ItemController {
+
+   private final ItemRepository itemRepository;
+
+
     //public  없으면 같은 패키지 안에서만 사용가능
     @GetMapping("/list")
     String list(Model model){
-        model.addAttribute("name","홍길동");
+       List<Item> result = itemRepository.findAll();
+       //List<출력중인 테이블 클래스 넣어야함 >
+        System.out.println(result.get(0).price);
+
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(30);
+        a.add(40);
+        System.out.println(a.get(0));
+        System.out.println(a.get(1));
+
+        model.addAttribute("name","비싼 바지");
         //      model.addAttribute("전달할데이터이름","데이터");
         return "list.html";
         //html에 서버데이터 넣어서 보내주려면 템플렌엔진 설치  (여기선 타임리프 사용)
