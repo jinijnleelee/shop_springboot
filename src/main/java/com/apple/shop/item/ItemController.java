7ab03f975cf2ed6f0845c5c09ab9d1a069f73589
LEE JINYOUNG <jinijnleelee@gmail.com>
2025-03-25 21:06:47 +0900

    ADD security

--- src/main/java/com/apple/shop/item/ItemController.java
+++ src/main/java/com/apple/shop/item/ItemController.java
@@ -3,6 +3,8 @@ package com.apple.shop.item;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +16,15 @@ import java.util.*;
 @Controller
 public class ItemController {
 
-   private final ItemRepository itemRepository;
-   private final ItemService itemService;
+    private final ItemRepository itemRepository;
+    private final ItemService itemService;
 
 
     //public  없으면 같은 패키지 안에서만 사용가능
     @GetMapping("/list")
-    String list(Model model){
-      List<Item> result = itemRepository.findAll();
-       //List<출력중인 테이블 클래스 넣어야함 >
+    String list(Model model) {
+        List<Item> result = itemRepository.findAll();
+        //List<출력중인 테이블 클래스 넣어야함 >
 //        System.out.println(result.get(0).price);
 //        ArrayList<Integer> a = new ArrayList<>();
 //        a.add(30);
@@ -37,27 +39,27 @@ public class ItemController {
 //     Age object = new Age();
 //      object.setAge(31);
 //      object.setName("jin");
-    //  object.OnePlusAge();
-      //object.SettingAge(12);
+        //  object.OnePlusAge();
+        //object.SettingAge(12);
 
 //        System.out.println(object.OnePlusAge());
 //        System.out.println(object.SettingAge(12));
 
 
-        model.addAttribute("items",result);
+        model.addAttribute("items", result);
         //      model.addAttribute("전달할데이터이름","데이터");
         return "list.html";
         //html에 서버데이터 넣어서 보내주려면 템플렌엔진 설치  (여기선 타임리프 사용)
         //서버 함수의 파라미터에  Model model 추가
         //model.addAttribute("전달할데이터이름","데이터값");
         //html에 가서 넣고싶은곳에 th:text="${name}" 적기
-        }
+    }
 
     @GetMapping("/write")
-    String write(){
-       // List<Item> result = itemRepository.findAll();
+    String write() {
+        // List<Item> result = itemRepository.findAll();
 
-     //   model.addAttribute("items",result);
+        //   model.addAttribute("items",result);
 
         return "write.html";
         //html에 서버데이터 넣어서 보내주려면 템플렌엔진 설치  (여기선 타임리프 사용)
@@ -68,24 +70,21 @@ public class ItemController {
 
     //public  없으면 같은 패키지 안에서만 사용가능
     @GetMapping("/detail/{id}")
-   String detail(@PathVariable("id") Long id,
-                  Model model)  {
+    String detail(@PathVariable("id") Long id,
+                  Model model) {
 
-        try{
-            Optional<Item>  reslut = itemRepository.findById(id);
+        try {
+            Optional<Item> reslut = itemRepository.findById(id);
             if (reslut.isPresent()) {
                 System.out.println(reslut.get());
-                model.addAttribute("data",reslut.get());
+                model.addAttribute("data", reslut.get());
                 return "detail.html";
-            }else {
-                return  "redirect:/list";
+            } else {
+                return "redirect:/list";
             }
         } catch (Exception e) {
-                return  "redirect:/list";
+            return "redirect:/list";
         }
-
-
-
 
 
 //        System.out.println(reslut.get());
@@ -95,18 +94,17 @@ public class ItemController {
 //        }
 
 
-
     }
+
     @PostMapping("/add")
 //    String addPost(@RequestParam Map formData){
     String addPost(
 //            @ModelAttribute Item item){ ModelAttribute로 통째로 담아주고 가져오는 방법도 있음
-    @RequestParam(name ="title") String title,
-                   @RequestParam(name ="price") int price){
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "price") int price) {
 
 
-    itemService.saveItem(title,price);
-
+        itemService.saveItem(title, price);
 
 
         //        Map<String,Object > test = new HashMap<>();
@@ -125,16 +123,16 @@ public class ItemController {
         return "redirect:/list";
 //product
     }
+
     @PostMapping("/editForm")
 //    String addPost(@RequestParam Map formData){
     String editForm(
-            @RequestParam(name ="title") String title,
-            @RequestParam(name ="price") int price,
-    Long id){
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "price") int price,
+            Long id) {
 
 
-        itemService.editForm(id,title,price);
-
+        itemService.editForm(id, title, price);
 
 
         //        Map<String,Object > test = new HashMap<>();
@@ -181,54 +179,59 @@ public class ItemController {
 
     @GetMapping("/edit/{id}")
     String editItems(@PathVariable("id") Long id,
-                     Model model)  {
-        try{
-            Optional<Item>  reslut = itemRepository.findById(id);
+                     Model model) {
+        try {
+            Optional<Item> reslut = itemRepository.findById(id);
             if (reslut.isPresent()) {
                 System.out.println(reslut.get());
-                model.addAttribute("data",reslut.get());
+                model.addAttribute("data", reslut.get());
 
 
                 return "edit.html";
-            }else {
-                return  "redirect:/list";
+            } else {
+                return "redirect:/list";
             }
         } catch (Exception e) {
-            return  "redirect:/list";
+            return "redirect:/list";
         }
 
     }
+
     @PostMapping("/test1")
-    String test1(@RequestBody Map<String, Object> name){
+    String test1(@RequestBody Map<String, Object> name) {
         // List<Item> result = itemRepository.findAll();
 
         //   model.addAttribute("items",result);
         System.out.println(name);
         System.out.println(name.get("name"));
-        return  "redirect:/list";
+        return "redirect:/list";
         //html에 서버데이터 넣어서 보내주려면 템플렌엔진 설치  (여기선 타임리프 사용)
         //서버 함수의 파라미터에  Model model 추가
         //model.addAttribute("전달할데이터이름","데이터값");
         //html에 가서 넣고싶은곳에 th:text="${name}" 적기
     }
+
     @GetMapping("/test2")
-    String test2(@RequestParam String name,int age){
+    String test2(@RequestParam String name, int age) {
         // List<Item> result = itemRepository.findAll();
 
         //   model.addAttribute("items",result);
         System.out.println(name);
         System.out.println(age);
-        return  "redirect:/list";
+        return "redirect:/list";
         //html에 서버데이터 넣어서 보내주려면 템플렌엔진 설치  (여기선 타임리프 사용)
         //서버 함수의 파라미터에  Model model 추가
         //model.addAttribute("전달할데이터이름","데이터값");
         //html에 가서 넣고싶은곳에 th:text="${name}" 적기
     }
 
-//    @PostMapping("/itemDelete")
-//    String itemDelete(){
-//        Optional<Item> result = itemRepository.findById(6L);
-//
+    @DeleteMapping("/itemDelete")
+    ResponseEntity<String> itemDelete(@RequestParam Long id) {
+        itemRepository.deleteById(id);
+
+        return ResponseEntity.status(200).body("삭제완료");
+
+
 //        if(result.isPresent()){
 //
 //            Long id = result.get().getId();
@@ -239,14 +242,20 @@ public class ItemController {
 //            System.out.println("응 맞아~");
 //            return  "redirect:/list";
 //        }
-//        return  "redirect:/list";
-//        //   model.addAttribute("items",result);
-//
-//
-//
-//
-//    }
+        // return  "redirect:/list";
+        //   model.addAttribute("items",result);
+
+
     }
+
+    @GetMapping("/test3")
+    String test3() {
+      var result = new BCryptPasswordEncoder().encode("ㅜ");
+        System.out.println(result);
+        return "redirect:/list";
+    }
+
+}
 
 
 
